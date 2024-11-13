62f5601ad23ec0fda44d7602b5711e8235258b2b
ATAN <natanahelatankeu@gmail.com>
2024-11-13 17:11:37 +0100

    500 Error occured

--- my-env/app.py
+++ my-env/app.py
@@ -3,8 +3,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/project')
+@app.route('/')
 def home():
+    raise Exception("Error 500")
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
